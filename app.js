1f2980c971ec8406b471bd84b8e1fa583ee70981
mfmontres <167644268+mfmontres@users.noreply.github.com>
2024-04-21 01:01:48 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    reset:true,
+    distance:'60px',
+    duration:2500,
+    delay:400
+  });
+  ScrollReveal().reveal('.section__subheader ,p', {delay:500, origin:'left',interval:100,delay:500});
+  ScrollReveal().reveal(' h1',{delay:600, origin:'right'});
+  ScrollReveal().reveal(' h4',{delay:600, origin:'bottom'});
+  
